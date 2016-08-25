a75eee8e05e69d03d69ac2005af48b37843d46bf
Miguel Ángel Martín <miguelbemartin@gmail.com>
2016-08-25 09:30:51 +0200

    Fix minor error

--- config/prod.php
+++ config/prod.php
@@ -3,7 +3,7 @@
 // Define vars
 $app['meetup.apitoken']      = '***********';
 $app['meetup.namecommunity'] = 'PHP-Sevilla';
-$app['meetup.host']          = 'http://api.meetup.com',
+$app['meetup.host']          = 'http://api.meetup.com';
 
-$app['twig.path'] = array(__DIR__.'/../templates/phpsevilla');
+$app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');

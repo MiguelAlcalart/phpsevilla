46c7b544c3aa355d2b4cfe6c801f1d1a799554ef
Miguel Ángel Martín <miguelbemartin@gmail.com>
2016-09-02 12:45:06 +0200

    Added new actions to the main controller class to navigate in the landing page

--- src/FrontendController.php
+++ src/FrontendController.php
@@ -25,22 +25,58 @@ $app->get('/', function () use ($app) {
 
     $events = $api->json();
 
+    $asistance = 0;
+    foreach ($events as $event) {
+        $asistance += $event['yes_rsvp_count'];
+    }
+
+    $api = $client->get($app['meetup.namecommunity'], [
+        'query' => [
+            'key'  => $app['meetup.apitoken'],
+        ]
+    ]);
+
+    $community = $api->json();
+
     $context = array(
-    	'events'     => $events,
+        'events'     => $events,
+        'community'  => $community,
+        'asistance'  => $asistance,
     );
 
     return $app['twig']->render('homepage.twig', $context);
 
 })->bind('index');
 
+/** 
+ * Community Controller
+ */
+$app->get('/community', function () use ($app) {
 
+    $context = array(
+        'event' => $event,
+    );
+
+    return $app['twig']->render('community.twig', $context);
+
+})->bind('community');
 
 /** 
  * Events Controller
  */
 $app->get('/events', function () use ($app) {
 
-	$events = array();
+	// Call API
+    $client = new Client(['base_url' => $app['meetup.host']]);
+
+    $api = $client->get($app['meetup.namecommunity'].'/events', [
+        'query' => [
+            'token'  => $app['meetup.apitoken'],
+            'status' => 'past',
+        ]
+    ]);
+
+    $events = $api->json();
 
     $context = array(
     	'events' => $events,
@@ -50,6 +86,40 @@ $app->get('/events', function () use ($app) {
 
 })->bind('events');
 
+/** 
+ * Events Controller
+ */
+$app->get('/events/{id}', function ($id) use ($app) {
+
+    $context = array(
+        'event' => $event,
+    );
+
+    return $app['twig']->render('event.twig', $context);
+
+})->bind('event');
+
+/** 
+ * Jobs Controller
+ */
+$app->get('/jobs', function () use ($app) {
+
+    $context = array(
+        'jobs' => $jobs,
+    );
+
+    return $app['twig']->render('jobs.twig', $context);
+
+})->bind('jobs');
+
+/** 
+ * Jobs Controller
+ */
+$app->get('/contact', function () use ($app) {
+
+    return $app['twig']->render('contact.twig');
+
+})->bind('contact');
 
 
 return $FrontendController;

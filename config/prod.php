6724367e73f9ed34ba58c0a3e53097ba24dfd89c
Miguel Ángel Martín <miguelbemartin@gmail.com>
2016-09-13 15:45:59 +0200

    Defined global vars

--- config/prod.php
+++ config/prod.php
@@ -1,9 +1,18 @@
 <?php
 
 // Define vars
-$app['meetup.apitoken']      = '***********';
-$app['meetup.namecommunity'] = 'PHP-Sevilla';
+$app['meetup.apitoken']      = '***';
 $app['meetup.host']          = 'http://api.meetup.com';
+$app['meetup.namecommunity'] = 'PHP-Sevilla';
+
+$app['twitter.consumer_key']              = '***';
+$app['twitter.consumer_secret']           = '***';
+$app['twitter.oauth_access_token']        = '***';
+$app['twitter.oauth_access_token_secret'] = '***';
+
+/* Information about community */
+$app['community.namecommunity'] = '***';
+$app['community.email']         = '***';
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
